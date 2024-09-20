248beda3a8f84596c3e83fd19fb2e48186593962
Thanak Mech <thanakmech@gmail.com>
2024-09-20 08:23:53 +0700

    i commit my code

--- src/app/page.js
+++ src/app/page.js
@@ -20,6 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
+          <li>Bonjour</li>
           <li>Save and see your changes instantly.</li>
         </ol>
 
